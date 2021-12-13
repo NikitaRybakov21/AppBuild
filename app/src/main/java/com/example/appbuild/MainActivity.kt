3b11b3d8a7b4d0af00106abb67b84b93d68f10d8
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-12-13 12:47:33 +0300

    ---DZ---Build----

--- app/src/main/java/com/example/appbuild/MainActivity.kt
+++ app/src/main/java/com/example/appbuild/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.appbuild
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+private lateinit var binding: ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+     // binding.textView.text = BuildConfig.TYPE
+        binding.textView.text = getString(R.string.hello)
     }
 }
